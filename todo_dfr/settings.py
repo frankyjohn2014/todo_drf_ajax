1f5596d5d8d692ebd559c5fe735bff18d13d1fb0
frankyjohn <frankyjohn2014@gmail.com>
2020-07-10 23:14:09 +0300

    3333333333

--- todo_dfr/settings.py
+++ todo_dfr/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'jgwmwie+lolz8)d5v^+gjtj-rb#@67amef)0^xr33ig&!*bi#s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['drf-ajax-drf.herokuapp.com','127.0.0.1'']
 
 
 # Application definition
