66e11b9e121f92c69a112e1228034e0f1a8733c5
frankyjohn <frankyjohn2014@gmail.com>
2020-07-10 21:22:55 +0300

    heroku2

--- todo_dfr/settings.py
+++ todo_dfr/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'jgwmwie+lolz8)d5v^+gjtj-rb#@67amef)0^xr33ig&!*bi#s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['drf-ajax-drf.herokuapp.com']
 
 
 # Application definition
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'todo_dfr.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['drf-ajax-drf.herokuapp.com','127.0.0.1'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
--- todo_dfr/urls.py
+++ todo_dfr/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('', include('frontend.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
